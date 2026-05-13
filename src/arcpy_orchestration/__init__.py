2f39918c794938ef6e1efaef7032e4278ea428e4
Joel McCune <joel5174@esri.com>
2026-05-13 14:23:15 -0700

    refactor(package): remove placeholder example_function and ExampleObject
    Strip the template scaffolding that was never part of the project's public API:
    
    - _main.py: remove example_function, ExampleObject, and the pandas import; leave the logger and comment stubs.
    
    - __init__.py: drop example_function / ExampleObject imports and __all__ entries; remove blank lines between dunder variables; drop the package-level logger init (module-level logger in _main.py is sufficient).

--- src/arcpy_orchestration/__init__.py
+++ src/arcpy_orchestration/__init__.py
@@ -1,21 +1,11 @@
 __title__ = "arcpy-orchestration"
 __version__ = "0.0.0"
 __author__ = "Joel McCune (https://github.com/knu2xs)"
-
 __license__ = "Apache 2.0"
-
 __copyright__ = "Copyright 2026 by Joel McCune (https://github.com/knu2xs)"
 
-# add specific imports below if you want to organize your code into modules, which is mostly what I do
-from . import config as config
+# add specific imports below if you want to organize your code into modules
+from . import config
 from . import utils
-from ._main import example_function, ExampleObject
 
-__all__ = ["config", "example_function", "ExampleObject", "utils"]
-
-# configure package-level logging
-logger = utils.get_logger(
-    "arcpy_orchestration",
-    level="DEBUG",
-    add_stream_handler=False,
-)
+__all__ = ["config", "utils"]
